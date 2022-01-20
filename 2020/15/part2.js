53d0dd4f7eb0b31d104c72f19c63e8727b680856
josephwarrick <nighthawk84756@gmail.com>
2022-01-20 14:45:40 -0600

    Add solutions for day 15 of year 2020.

---
+++ 2020/15/part2.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const fs = require('fs');
+
+const startingNumbers = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('\n')[0].split(',').map(Number);
+const lastSpokenAt = {};
+let lastSpoken;
+let turn = 1;
+
+function speak(num) {
+  lastSpokenAt[num] = lastSpokenAt[num] || [];
+  lastSpokenAt[num].push(turn);
+  lastSpokenAt[num] = lastSpokenAt[num].slice(-2);
+  lastSpoken = num;
+  turn++;
+}
+
+startingNumbers.forEach(speak);
+
+while (turn <= 3e7) {
+  if (lastSpokenAt[lastSpoken].length === 1) {
+    speak(0);
+  } else {
+    const turnDistance = lastSpokenAt[lastSpoken][1] - lastSpokenAt[lastSpoken][0];
+    speak(turnDistance);
+  }
+}
+
+console.log(lastSpoken);
